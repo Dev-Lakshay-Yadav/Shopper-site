31b517ea76443e313970716ce09c90e3a3193e40
lakshay2808 <lakshay8219@gmail.com>
2024-06-06 14:10:16 +0530

    changes

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,6 +1,22 @@
+
 const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div className="loginsignup">
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder="Your name"/>
+          <input type="email" placeholder="Email address"/>
+          <input type="password" placeholder="Password"/>
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">Already have an account?<span>Login here</span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id=""/>
+          <p>by continuing, i agree to the terms of use & privacy policy  </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],  theme: {
     extend: {
+      gridTemplateColumns: {
+        'custom': '0.5fr 2fr 1fr 1fr 1fr 1fr',
+      },
       boxShadow: {
         'custom': '0 1px 3px -2px black',
       }
